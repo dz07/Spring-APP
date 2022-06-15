8cd528d6cac3a900acb1123ac5f9cdded3d18637
Diab Zohaîr <zohairdiab07@gmail.com>
2022-06-15 21:18:36 +0100

    Roger git status

---
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -0,0 +1,20 @@
+
+package org.springframework.samples.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PetClinic Spring Boot Application.
+ *
+ * @author Dave Syer
+ *
+ */
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
